920933cc3388b47c54cd896f50ebc5e1553eecdb
HKLCF <hklcfs@gmail.com>
2020-06-03 19:29:32 +0800

    Update index.php

--- ott/index.php
+++ ott/index.php
@@ -6,8 +6,8 @@
     Last Modified: 09/04/2016
 */
 $login_ch = curl_init();
-$login_data = '{"channelno":"099","deviceId":"0000anonymous_user","format":"HLS"}';
-curl_setopt($login_ch, CURLOPT_URL, 'http://api.viu.now.com/p8/1/getLiveURL');
+$login_data = '{"contentId":"099","deviceId":"0000anonymous_user","deviceType":"IOS_PHONE"}';
+curl_setopt($login_ch, CURLOPT_URL, 'http://api.viu.now.com/p8/3/getLiveURL');
 curl_setopt($login_ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($login_ch, CURLOPT_POST, true);
 curl_setopt($login_ch, CURLOPT_POSTFIELDS, $login_data);
